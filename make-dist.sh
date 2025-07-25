6d4173a4245d375967c0df9bfdde5a5dbc2ef54d
Becky Nelson <dekarrin.irl@gmail.com>
2025-07-25 08:47:09 -0500

    make release script v agnostic

--- make-dist.sh
+++ make-dist.sh
@@ -10,8 +10,15 @@ set -euo pipefail
 
 # if version was given, use that; otherwise, use the current git commit hash
 git_commit="$(git rev-parse --short HEAD)"
-VERSION="${1:-$git_commit}"
-DIST_DIR="szuru-admin-plus-v$VERSION"
+VERSION="${1:-.}"
+if [[ "$VERSION" == "." ]]; then
+    VERSION="$git_commit"
+elif [[ "${VERSION:0:1}" != "v" ]]; then
+    VERSION="v$VERSION"
+fi
+
+
+DIST_DIR="szuru-admin-plus-$VERSION"
 
 mkdir -p "$DIST_DIR"
 mkdir -p "$DIST_DIR/admin-dist"
