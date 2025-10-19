065558fd5a28b1540e6e1437a49abd2aa18fc390
Deka Jello <dekarrin.irl@gmail.com>
2025-10-19 12:24:45 -0500

    #8: first update to script not tested

--- scripts/fix-exif-rotations.sh
+++ scripts/fix-exif-rotations.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+function fix_file() {
+	f="$1"
+	
+	if [ ! -f "$f" ]
+	then
+		echo "'$f' does not exist. Skipping" >&2
+		return 1
+	fi
+	
+	cur="$(exiftran -d "$f" 2>/dev/null | grep 'Orientation' | head -n 1 | awk '{print $3;}')"
+	
+	#formatting
+	cur="$(echo "$cur" | xargs | tr '[:upper:]' '[:lower:]')"
+	
+	# skip cases that dont need fixing
+	[ -n "$cur" ] || return 1
+	[ "$cur" != 'top-left' ] || return 1
+
+	id="$(echo "$(basename "$f")" | cut -d _ -f 1)"
+	
+	echo "Rotating file for post ID $id..."
+	exiftran -a -i -p "$f"
+	return 0
+}
+
 BOORU_DIR="$(dirname "$0")"
 
 files="$1"
@@ -12,32 +37,101 @@ then
 fi
 
 
+# scan for different input modes
+input_mode_files=1
+input_mode_id_range=2
+
+input_mode="$input_mode_files"
+
+for f in "$@" ; do
+	if [ "$f" = "--id-range" ]
+	then
+		input_mode="$input_mode_id_range"
+	fi
+done
+
+
 any_fixed=
 id_arg_str=
-for f in "$@" ; do
-	if [ ! -f "$f" ]
+
+if [ "$input_mode" -eq "$input_mode_id_range" ]
+then
+	SCRIPT_DIR="$(dirname "$0")"
+
+	if [ "$#" -ne 3 -a "$#" -ne 2 ]
 	then
-		echo "'$f' does not exist. Skipping" >&2
-		continue
+		echo 'error: --id-range input mode requires exactly one or two args' >&2
+		echo "usage: $0 --id-range OLDEST [NEWEST]" >&2
+		exit 1
 	fi
-	
-	cur="$(exiftran -d "$f" 2>/dev/null | grep 'Orientation' | head -n 1 | awk '{print $3;}')"
-	
-	#formatting
-	cur="$(echo "$cur" | xargs | tr '[:upper:]' '[:lower:]')"
-	
-	# skip cases that dont need fixing
-	[ -n "$cur" ] || continue
-	[ "$cur" != 'top-left' ] || continue
-	
-	f_name="$(basename "$f")"
-	id="$(echo "$f_name" | cut -d _ -f 1)"
-	id_arg_str="$id_arg_str $id"
-	
-	echo "Rotating file for post ID $id..."
-	exiftran -a -i -p "$f"
-	any_fixed=1
-done
+
+	oldest=
+	newest=
+
+	set_oldest=
+	digit_re='^[0-9]$'
+	for arg in "$@"; do
+		[ "$arg" != "--id-range" ] || continue
+		if [ -z "$set_oldest" ]
+		then
+			if ! [[ "$arg" =~ $digit_re ]]
+			then
+				echo "error: not a number: $arg" >&2
+				exit 2
+			fi
+			oldest="$arg"
+			set_oldest=1
+		else
+			if ! [[ "$arg" =~ $digit_re ]]
+			then
+				echo "error: not a number: $arg" >&2
+				exit 2
+			fi
+			newest="$arg"
+		fi
+	done
+
+	posts_path="$SCRIPT_DIR/data/posts"
+	if [ -z "$newest" ]
+	then
+		# auto-detect highest post number
+		echo "Checking for highest post number..."
+		check=$(( $oldest + 1 ))
+		while true
+		do
+			f="$posts_path/$check"*
+			if [ "$f" -eq "$posts_path/$check*" ]
+				newest=$(( $check - 1 ))
+				break
+			fi
+			(( $check++ ))
+		done
+		echo "Got $newest"
+	fi
+
+	for pid in {$oldest..$newest}
+	do
+		f="$posts_path/$pid"*
+		if [ "$f" -eq "$posts_path/$pid*" ]
+			continue
+		fi
+
+		if fix_file "$f"
+		then
+			id_arg_str="$id_arg_str $pid"
+			any_fixed=1
+		fi
+	done
+else
+	for f in "$@" ; do
+		if fix_file "$f"
+		then
+			id="$(basename "$f" | cut -d _ -f 1)"
+			id_arg_str="$id_arg_str $id"
+			any_fixed=1
+		fi
+	done
+fi
 
 if [ -z "$any_fixed" ]
 then
